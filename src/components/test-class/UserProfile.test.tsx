7dbed3cea6e7cd98a2dfb4c6dffcd3e0bfe7a1e1
devkoow <cwl64658@gmail.com>
2025-05-13 14:42:13 +0900

    [fix] 테스트 린트 에러

--- src/components/test-class/UserProfile.test.tsx
+++ src/components/test-class/UserProfile.test.tsx
@@ -6,11 +6,9 @@ test('요소들이 렌더링되는지 확인', () => {
 
     const container = screen.getByTestId('profile-container');
     const heading = screen.getByRole('heading', { name: 'Wook님의 프로필' })
-    const avatar = screen.getByAltText('프로필 이미지');
     const button = screen.getByRole('button', { name: '프로필 수정' });
 
     expect(container).toHaveClass('verified');
     expect(heading).toHaveTextContent('Wook님의 프로필');
-    expect(avatar).toBeInTheDocument();
     expect(button).not.toBeDisabled();
 })
